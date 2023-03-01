9fc79c12e9cb570f94416ef1d1ff1d4d68557ad4
Tarek Alabd <tarekalabd1@gmail.com>
2023-03-01 12:15:07 +0200

    feat: integrate the bottomNavBar with the pages

--- lib/pages/bottom_navbar.dart
+++ lib/pages/bottom_navbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/pages/home_page.dart';
 
 class BottomNavBarPage extends StatefulWidget {
   const BottomNavBarPage({super.key});
@@ -16,11 +17,30 @@ class _BottomNavBarPageState extends State<BottomNavBarPage> {
     });
   }
 
+  List<Widget> bodyOptions = const [
+    HomePage(),
+    Center(child: Text('Favorite Page'),),
+    Center(child: Text('Account Page'),),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('Foodak'),
+        backgroundColor: Colors.grey[100],
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: bodyOptions[selectedIndex],
+      drawer: const Drawer(
+        child: Center(
+          child: Text('Iam in the drawer!'),
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
@@ -35,6 +55,7 @@ class _BottomNavBarPageState extends State<BottomNavBarPage> {
           ),
         ],
         currentIndex: selectedIndex,
+        selectedItemColor: Colors.deepOrange,
         onTap: onItemTapped,
       ),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,47 +7,33 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        title: const Text('Foodak'),
-        backgroundColor: Colors.grey[100],
-        foregroundColor: Colors.black,
-        elevation: 0,
-      ),
-      drawer: const Drawer(
-        child: Center(
-          child: Text('Iam in the drawer!'),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(24.0),
-              child: Image.asset(
-                'assets/images/classic_burger.jpg',
-                height: 200,
-                fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(24.0),
+            child: Image.asset(
+              'assets/images/classic_burger.jpg',
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 32.0),
+          Expanded(
+            child: GridView.builder(
+              itemCount: food.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+              ),
+              itemBuilder: (context, index) => FoodGridItem(
+                foodItem: food[index],
               ),
             ),
-            const SizedBox(height: 32.0),
-            Expanded(
-              child: GridView.builder(
-                itemCount: food.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 20,
-                  crossAxisSpacing: 20,
-                ),
-                itemBuilder: (context, index) => FoodGridItem(
-                  foodItem: food[index],
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
