2792a4d0b540b30df83f43569c04722b39df8d2b
Tarek Alabd <tarekalabd1@gmail.com>
2023-03-05 00:59:26 +0200

    feat: building and styling texts

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -3,6 +3,27 @@ import 'package:flutter/material.dart';
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
 
+  Widget orderVoucherItem({required String name, required int number}) {
+    return Column(
+      children: [
+        Text(
+          number.toString(),
+          style: const TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w600,
+            color: Colors.deepOrange,
+          ),
+        ),
+        Text(
+          name,
+          style: const TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -19,6 +40,22 @@ class AccountPage extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
+          ),
+          const SizedBox(height: 16.0),
+          const Text(
+            'Tarek Alabd',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              orderVoucherItem(name: 'Orders', number: 50),
+              orderVoucherItem(name: 'Vouchers', number: 10),
+            ],
           ),
         ],
       ),
