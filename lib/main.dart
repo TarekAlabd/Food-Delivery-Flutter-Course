536452ada99d41597da7432e7d93fb7ceadab43b
Tarek Alabd <tarekalabd1@gmail.com>
2023-03-05 12:52:32 +0200

    feat: overview of themes

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Foodak - Food Delivery',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        
       ),
       home: const BottomNavBarPage(),
     );
