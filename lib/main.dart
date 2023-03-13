7521a9bbf4770be43109ff265012f82388fdd63d
Tarek Alabd <tarekalabd1@gmail.com>
2023-03-13 14:30:13 +0200

    feat: controlling the device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:food_delivery/pages/bottom_navbar.dart';
-import 'package:food_delivery/pages/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
