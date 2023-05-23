fa2897e39e42a5238e41c00b006f93dcf96efba5
Tarek Alabd <tarekalabd1@gmail.com>
2023-05-23 17:33:44 +0300

    feat: adding custom scrollview and slivers

--- lib/pages/food_details_page.dart
+++ lib/pages/food_details_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/models/food_item.dart';
+import 'package:food_delivery/widgets/custom_back_button.dart';
+import 'package:food_delivery/widgets/favorite_button.dart';
 import 'package:food_delivery/widgets/food_details/food_item_counter.dart';
 import 'package:food_delivery/widgets/food_details/property_item.dart';
-import 'package:food_delivery/widgets/food_details/top_banner.dart';
 
 class FoodDetailsPage extends StatelessWidget {
   final int foodIndex;
@@ -20,91 +21,123 @@ class FoodDetailsPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    TopBanner(foodIndex: foodIndex),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 16.0,
-                        right: 16.0,
-                        top: 16.0,
-                        bottom: 46.0,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    food[foodIndex].name,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headlineSmall!
-                                        .copyWith(
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                  ),
-                                  const SizedBox(height: 6.0),
-                                  Text(
-                                    'Buffalo Burger',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyLarge!
-                                        .copyWith(
-                                          color: Colors.grey,
-                                        ),
-                                  ),
-                                ],
-                              ),
-                              FoodItemCounter(),
-                            ],
-                          ),
-                          const SizedBox(height: 32.0),
-                          IntrinsicHeight(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
-                                PropertyItem(
-                                    propertyName: 'Size',
-                                    propertyValue: 'Medium'),
-                                VerticalDivider(
-                                  indent: 0,
-                                  endIndent: 0,
-                                ),
-                                PropertyItem(
-                                    propertyName: 'Calories',
-                                    propertyValue: '150 Kcal'),
-                                VerticalDivider(
-                                  indent: 0,
-                                  endIndent: 0,
-                                ),
-                                PropertyItem(
-                                    propertyName: 'Cooking',
-                                    propertyValue: '10-20 Min'),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 16.0),
-                          Text(
-                            'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium!
-                                .copyWith(
-                                  color: Colors.grey,
-                                ),
-                          ),
-                          // const SizedBox(height: 32.0),
-                        ],
+              child: CustomScrollView(
+                slivers: [
+                  SliverAppBar(
+                    leading: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CustomBackButton(
+                        width: size.width * 0.09,
+                        height: size.height * 0.04,
                       ),
                     ),
-                  ],
-                ),
+                    actions: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FavoriteButton(
+                          foodIndex: foodIndex,
+                          width: size.width * 0.1,
+                          height: size.height * 0.04,
+                        ),
+                      ),
+                    ],
+                    expandedHeight: size.height * 0.35,
+                    pinned: true,
+                    flexibleSpace: FlexibleSpaceBar(
+                      background: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.15),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 24.0),
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Image.network(
+                              food[foodIndex].imgUrl,
+                              fit: BoxFit.contain,
+                              height: size.height * 0.28,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SliverPadding(
+                    padding: const EdgeInsets.only(
+                      left: 16.0,
+                      right: 16.0,
+                      top: 16.0,
+                      bottom: 46.0,
+                    ),
+                    sliver: SliverList(
+                      delegate: SliverChildListDelegate([
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  food[foodIndex].name,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineSmall!
+                                      .copyWith(
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                ),
+                                const SizedBox(height: 6.0),
+                                Text(
+                                  'Buffalo Burger',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge!
+                                      .copyWith(
+                                        color: Colors.grey,
+                                      ),
+                                ),
+                              ],
+                            ),
+                            const FoodItemCounter(),
+                          ],
+                        ),
+                        const SizedBox(height: 32.0),
+                        const IntrinsicHeight(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              PropertyItem(
+                                  propertyName: 'Size',
+                                  propertyValue: 'Medium'),
+                              VerticalDivider(
+                                indent: 0,
+                                endIndent: 0,
+                              ),
+                              PropertyItem(
+                                  propertyName: 'Calories',
+                                  propertyValue: '150 Kcal'),
+                              VerticalDivider(
+                                indent: 0,
+                                endIndent: 0,
+                              ),
+                              PropertyItem(
+                                  propertyName: 'Cooking',
+                                  propertyValue: '10-20 Min'),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 16.0),
+                        Text(
+                          'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem',
+                          style:
+                              Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                    color: Colors.grey,
+                                  ),
+                        ),
+                      ]),
+                    ),
+                  ),
+                ],
               ),
             ),
             Padding(
