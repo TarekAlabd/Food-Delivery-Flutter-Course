28b101d4f72d7b777b458e491f181e8b8db2c3f2
Tarek Alabd <tarekalabd1@gmail.com>
2023-05-23 16:29:04 +0300

    feat: make the bottom always appear in food details page

--- lib/pages/food_details_page.dart
+++ lib/pages/food_details_page.dart
@@ -14,104 +14,120 @@ class FoodDetailsPage extends StatelessWidget {
 
     return Scaffold(
       // appBar: AppBar(),
-      body: SingleChildScrollView(
+      body: SafeArea(
+        top: false,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TopBanner(foodIndex: foodIndex),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 16.0,
-                right: 16.0,
-                top: 16.0,
-                bottom: 46.0,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TopBanner(foodIndex: foodIndex),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16.0,
+                        right: 16.0,
+                        top: 16.0,
+                        bottom: 46.0,
+                      ),
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    food[foodIndex].name,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineSmall!
+                                        .copyWith(
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                  ),
+                                  const SizedBox(height: 6.0),
+                                  Text(
+                                    'Buffalo Burger',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyLarge!
+                                        .copyWith(
+                                          color: Colors.grey,
+                                        ),
+                                  ),
+                                ],
+                              ),
+                              FoodItemCounter(),
+                            ],
+                          ),
+                          const SizedBox(height: 32.0),
+                          IntrinsicHeight(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: const [
+                                PropertyItem(
+                                    propertyName: 'Size',
+                                    propertyValue: 'Medium'),
+                                VerticalDivider(
+                                  indent: 0,
+                                  endIndent: 0,
+                                ),
+                                PropertyItem(
+                                    propertyName: 'Calories',
+                                    propertyValue: '150 Kcal'),
+                                VerticalDivider(
+                                  indent: 0,
+                                  endIndent: 0,
+                                ),
+                                PropertyItem(
+                                    propertyName: 'Cooking',
+                                    propertyValue: '10-20 Min'),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 16.0),
                           Text(
-                            food[foodIndex].name,
+                            'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem',
                             style: Theme.of(context)
                                 .textTheme
-                                .headlineSmall!
+                                .bodyMedium!
                                 .copyWith(
-                                  fontWeight: FontWeight.w600,
+                                  color: Colors.grey,
                                 ),
                           ),
-                          const SizedBox(height: 6.0),
-                          Text(
-                            'Buffalo Burger',
-                            style:
-                                Theme.of(context).textTheme.bodyLarge!.copyWith(
-                                      color: Colors.grey,
-                                    ),
-                          ),
+                          // const SizedBox(height: 32.0),
                         ],
                       ),
-                      FoodItemCounter(),
-                    ],
-                  ),
-                  const SizedBox(height: 32.0),
-                  IntrinsicHeight(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        PropertyItem(
-                            propertyName: 'Size', propertyValue: 'Medium'),
-                        VerticalDivider(
-                          indent: 0,
-                          endIndent: 0,
-                        ),
-                        PropertyItem(
-                            propertyName: 'Calories',
-                            propertyValue: '150 Kcal'),
-                        VerticalDivider(
-                          indent: 0,
-                          endIndent: 0,
-                        ),
-                        PropertyItem(
-                            propertyName: 'Cooking',
-                            propertyValue: '10-20 Min'),
-                      ],
                     ),
-                  ),
-                  const SizedBox(height: 16.0),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 16.0, left: 16.0, right: 16.0),
+              child: Row(
+                children: [
                   Text(
-                    'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Colors.grey,
+                    '\$ ${food[foodIndex].price}',
+                    style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.w600,
                         ),
                   ),
-                  const SizedBox(height: 32.0),
-                  Row(
-                    children: [
-                      Text(
-                        '\$ ${food[foodIndex].price}',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineMedium!
-                            .copyWith(
-                              color: Theme.of(context).primaryColor,
-                              fontWeight: FontWeight.w600,
-                            ),
+                  const SizedBox(width: 46.0),
+                  Expanded(
+                    child: SizedBox(
+                      height: size.height * 0.058,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Checkout'),
                       ),
-                      const SizedBox(width: 46.0),
-                      Expanded(
-                        child: SizedBox(
-                          height: size.height * 0.058,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: const Text('Checkout'),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ],
               ),
